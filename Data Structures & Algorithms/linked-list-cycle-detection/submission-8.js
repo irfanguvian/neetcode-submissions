e01a4a084c457425d771f40bcfcb11b3e831d291
Irfan Muhammad Guvian <55593637+irfanguvian@users.noreply.github.com>
2026-08-12 14:21:44 +0700

    Add: linked-list-cycle-detection - submission-8

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-8.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+        const uniqueNodes = new Set()
+
+        let current = head
+
+        while(current) {
+            if(uniqueNodes.has(current)) {
+                return true
+            }
+            uniqueNodes.add(current)
+            current = current.next
+        }
+
+        return false
+    }
+}
